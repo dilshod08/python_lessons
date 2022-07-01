67a5cb6b7fff404abc155a5824331780b7f88317
dilshod08 <bakhritdinovd@gmail.com>
2022-07-01 11:36:02 -0700

    2-dars

---
+++ 2-dars/dars.py
@@ -0,0 +1,17 @@
+print("""Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami""")
+print('''Odami ersang, demagil odami,
+Oniki, yo'q xalq g'amidin g'ami''')
+print("Odami ersang, demagil odami,\nOniki, yo'q xalq g'amidin g'ami")
+print('Odami ersang, demagil odami,\nOniki, yo\'q xalq g\'amidin g\'ami')
+print(2+4*2)
+print(19/5)
+print(20/4)
+print(19//5)
+print(20//4)
+print(2**4)
+print(21%4)
+print(21%5)
+print("To'qqizning kvadrati", 9**2, "ga teng")
+print('3x3=',3*3)
+
