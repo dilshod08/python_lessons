c80db6d4db248020ef43942141cfcca966c6c560
dilshod08 <bakhritdinovd@gmail.com>
2022-07-04 07:52:48 -0700

    12-dars

---
+++ 12-dars/dars.py
@@ -0,0 +1,3 @@
+# Kodni ishga tushirib darsda qilingan vazifalarni ko'rishingiz mumkin.
+import webbrowser
+url = webbrowser.open('https://github.com/dilshod08')
---
+++ 12-dars/work.py
@@ -0,0 +1,3 @@
+# Kodni ishga tushirib uyga vazifalarni ko'rishingiz mumkin.
+import webbrowser
+url = webbrowser.open('https://github.com/dilshod08/python_lessons')
